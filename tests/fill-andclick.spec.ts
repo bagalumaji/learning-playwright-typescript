e84079afd013040a6fad9c1d5ccd93ba16ab1d98
Umaji Bagal <bagalumaji@gmail.com>
2024-09-21 11:03:21 +0530

    fill and click test

---
+++ tests/fill-andclick.spec.ts
@@ -0,0 +1,19 @@
+import {test, expect, Locator} from "@playwright/test";
+
+test.describe("fill and click", () => {
+    test("fill and click button test", async ({page}) => {
+        await page.goto("https://www.lambdatest.com/selenium-playground/");
+        await page.getByRole('link',{name:"Ajax Form Submit"}).click()
+        await expect(page.getByRole('heading',{name:'Form Submit Demo'})).toBeVisible({timeout:15000});
+        const textBoxTitle=page.locator("id=title");
+        const textBoxDescription=page.locator("id=description");
+        await  textBoxTitle.fill("sayaji");
+        await  textBoxDescription.fill("sharannya");
+        const titleValue = await textBoxTitle.inputValue();
+        const descriptionValue = await textBoxDescription.inputValue();
+        console.log(titleValue);
+        console.log(descriptionValue);
+        await page.getByRole("button",{name:"submit"}).click()
+        await expect(page.locator("id=submit-control")).toContainText("Ajax Request is Processing!");
+    })
+})
