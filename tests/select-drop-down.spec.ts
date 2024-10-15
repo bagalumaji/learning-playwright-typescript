4d39c2382e23f9fdf394bff86b15659ad14ce3f5
umaji bagal <bagalumaji@gmail.com>
2024-10-15 09:14:36 +0530

    added code for verification

--- tests/select-drop-down.spec.ts
+++ tests/select-drop-down.spec.ts
@@ -1,19 +1,23 @@
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test("select single drop down test",async ({page})=>{
+test.only("select single drop down test", async ({page}) => {
     await page.goto("https://demoqa.com/select-menu")
-    const oldSelect= page.locator("id=oldSelectMenu");
+    const oldSelect = page.locator("id=oldSelectMenu");
     await oldSelect.selectOption("4");
     await page.waitForTimeout(2000);
+    let text = await oldSelect.inputValue();
+    expect(text).toBe("4");
     await oldSelect.selectOption("Black");
     await page.waitForTimeout(2000);
-    await oldSelect.selectOption({index:6});
+    text = await oldSelect.locator('option:checked').textContent();
+    expect(text).toBe("Black");
+    await oldSelect.selectOption({index: 6});
     await page.waitForTimeout(3000)
 })
-test.only("multi select drop down test",async ({page})=>{
+test("multi select drop down test", async ({page}) => {
     await page.goto("https://demoqa.com/select-menu");
-    const multiSelectDropDown=page.locator("id=cars");
-    await multiSelectDropDown.selectOption([{index:0},{value:"saab"},{label:"Audi"}])
+    const multiSelectDropDown = page.locator("id=cars");
+    await multiSelectDropDown.selectOption([{index: 0}, {value: "saab"}, {label: "Audi"}])
     await page.waitForTimeout(2000);
     await multiSelectDropDown.selectOption([]);
     await page.waitForTimeout(5000);
@@ -21,12 +25,12 @@ test.only("multi select drop down test",async ({page})=>{
 
     //multi select using labels
 
-    const labels=["Opel","Audi"];
+    const labels = ["Opel", "Audi"];
     await multiSelectDropDown.selectOption(labels);
     await page.waitForTimeout(3000);
 
     //select by using values
-    const values=["volvo","saab"];
+    const values = ["volvo", "saab"];
     await multiSelectDropDown.selectOption(values);
 
     await page.waitForTimeout(3000);
